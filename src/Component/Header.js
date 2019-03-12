358919d7841eaf426ce2901416854f8bd56ac865
Manngold <jsw4820@gmail.com>
2019-03-12 16:58:07 +0900

    styled component setting

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,17 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+// css파일을 모듈화 하여 임의의(randomize) 클래스로 변경하여 사용
 
+const Header = styled.header``;
+
+const List = styled.ul`
+  display: flex;
+  &:hover {
+    background-color: blue;
+  }
+`;
+
+const Item = styled.li``;
+
+const SLink = styled(Link)``;
 export default () => (
-  <header>
-    <ul>
-      <li>
-        <a href="/">Home</a>
-      </li>
-      <li>
-        <a href="/tv">TV</a>
-      </li>
-      <li>
-        <a href="/search">Search</a>
-      </li>
-    </ul>
-  </header>
+  <Header>
+    <List>
+      <Item>
+        <Link to="/">Home</Link>
+      </Item>
+      <Item>
+        <Link to="/tv">TV</Link>
+      </Item>
+      <Item>
+        <Link to="/search">Search</Link>
+      </Item>
+    </List>
+  </Header>
 );
--- src/Component/Router.js
+++ src/Component/Router.js
@@ -8,20 +8,23 @@ import {
 import Home from "../Routes/Home";
 import Search from "../Routes/Search";
 import TV from "../Routes/TV";
-
+import Header from "./Header";
 export default () => (
   <Router>
     <>
       <Switch>
         {/*rendering only one router*/}
-        <Route path="/" exact component={Home} />
-        <Route path="/tv" exact component={TV} />
-        <Route
-          path="/tv/popular"
-          render={() => <h1>composition rendering</h1>}
-        />
-        <Route path="/search" component={Search} />
-        <Redirect from="*" to="/" />
+        <>
+          <Header />
+          <Route path="/" exact component={Home} />
+          <Route path="/tv" exact component={TV} />
+          <Route
+            path="/tv/popular"
+            render={() => <h1>composition rendering</h1>}
+          />
+          <Route path="/search" component={Search} />
+          <Redirect from="*" to="/" />
+        </>
       </Switch>
     </>
   </Router>
