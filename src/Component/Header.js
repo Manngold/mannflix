f53df182939b6c4456b0fe03e2c5cfae85245649
Manngold <jsw4820@gmail.com>
2019-03-14 16:02:16 +0900

    get props using withRouter

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import styled from "styled-components";
 // css파일을 모듈화 하여 임의의(randomize) 클래스로 변경하여 사용
 
@@ -11,22 +11,23 @@ const Header = styled.header`
   width: 100%;
   height: 50px;
   display: flex;
-  align-items: center;
   padding: 0px 10px;
+  align-items: center;
   background-color: rgb(20, 20, 20, 0.8);
+  z-index: 10;
   box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
 `;
 
 const List = styled.ul`
   display: flex;
-  &:hover {
-    background-color: blue;
-  }
 `;
 
 const Item = styled.li`
   width: 50px;
   text-align: center;
+  border-bottom: 5px solid
+    ${props => (props.current ? "#3498db" : "transparent")};
+  transition: border-bottom 0.5s ease-in-out;
 `;
 
 const SLink = styled(Link)`
@@ -35,18 +36,19 @@ const SLink = styled(Link)`
   align-items: center;
   justify-content: center;
 `;
-export default () => (
+
+export default withRouter(({ location: { pathname } }) => (
   <Header>
     <List>
-      <Item>
+      <Item current={pathname === "/" ? true : false}>
         <SLink to="/">Movies</SLink>
       </Item>
-      <Item>
+      <Item current={pathname === "/tv" ? true : false}>
         <SLink to="/tv">TV</SLink>
       </Item>
-      <Item>
+      <Item current={pathname === "/search" ? true : false}>
         <SLink to="/search">Search</SLink>
       </Item>
     </List>
   </Header>
-);
+));
