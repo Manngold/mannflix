4d13f2565a7f4ff8d7fbfa12567e6ac5b3416d4c
Manngold <jsw4820@gmail.com>
2019-03-18 22:04:58 +0900

    Home Container

--- src/Component/Router.js
+++ src/Component/Router.js
@@ -5,26 +5,25 @@ import {
   Redirect,
   Switch
 } from "react-router-dom";
-import Home from "../Routes/Home";
-import Search from "../Routes/Search";
-import TV from "../Routes/TV";
+import Home from "../Routes/Home/index";
+import Search from "../Routes/Search/index";
+import TV from "../Routes/TV/index";
 import Header from "./Header";
 export default () => (
   <Router>
     <>
+      {/*rendering only one router*/}
+
+      <Header />
       <Switch>
-        {/*rendering only one router*/}
-        <>
-          <Header />
-          <Route path="/" exact component={Home} />
-          <Route path="/tv" exact component={TV} />
-          <Route
-            path="/tv/popular"
-            render={() => <h1>composition rendering</h1>}
-          />
-          <Route path="/search" component={Search} />
-          <Redirect from="*" to="/" />
-        </>
+        <Route path="/" exact component={Home} />
+        <Route path="/tv" exact component={TV} />
+        <Route
+          path="/tv/popular"
+          render={() => <h1>composition rendering</h1>}
+        />
+        <Route path="/search" component={Search} />
+        <Redirect from="*" to="/" />
       </Switch>
     </>
   </Router>
