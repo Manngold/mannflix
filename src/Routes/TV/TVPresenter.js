b396b9de666402a57efbf62744d85f894606af00
Manngold <jsw4820@gmail.com>
2019-03-29 22:10:35 +0900

    Home presenter and Section component

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -5,7 +5,7 @@ import styled from "styled-components";
 const TVPresenter = ({ topRated, popular, airingToday, loading, error }) =>
   null;
 
-TVPresenter.PropTypes = {
+TVPresenter.propTypes = {
   topRated: PropTypes.array,
   popular: PropTypes.array,
   airingToday: PropTypes.array,
