0a9fbf142d16c93cbd4eba4a416c56d11c3a19d2
Manngold <jsw4820@gmail.com>
2019-04-08 17:25:02 +0900

    Detail presenter

--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -23,6 +23,38 @@ const Backdrop = styled.div`
   opacity: 0.5;
 `;
 
+const Content = styled.div`
+  display: flex;
+  width: 100%;
+  position: relative;
+  z-index: 1;
+  height: 100%;
+  bodrder-radius: 5px;
+`;
+
+const Cover = styled.div`
+  height: 100%;
+  width: 30%;
+  background-image: url(${props => props.bgImage});
+  background-position: center center;
+  background-size: cover;
+`;
+
+const Data = styled.div`
+  width: 70%;
+  margin-left: 10px;
+`;
+
+const Title = styled.span``;
+
+const ItemContainer = styled.div``;
+
+const Item = styled.span``;
+
+const Divider = styled.span``;
+
+const Overview = styled.p``;
+
 const DetailPresenter = ({ result, loading, error }) =>
   loading ? (
     <Loader />
@@ -31,6 +63,19 @@ const DetailPresenter = ({ result, loading, error }) =>
       <Backdrop
         bgImage={`https://image.tmdb.org/t/p/original${result.backdrop_path}`}
       />
+      <Content>
+        <Cover
+          bgImage={
+            result.poster_path
+              ? `https://image.tmdb.org/t/p/original${result.poster_path}`
+              : require("../../assets/noPoster.png")
+          }
+        />
+        <Data>
+          <Title />
+          <ItemContainer />
+        </Data>
+      </Content>
     </Container>
   );
 
